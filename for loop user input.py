79ec3666c424107be2de9b6d8cf096eb284c4e27
mco6219 <mco6219@stacmail.net>
2020-02-13 11:31:34 +1300

    Create for loop user input.py

---
+++ for loop user input.py
@@ -0,0 +1,10 @@
+""" this program gets two numbers from a user, then counts up from the lower to the higher """
+
+# ask user input for two numbers
+number_one = int(input("Enter a small number"))
+number_two = int(input("Enter a large number"))
+
+if counter in range(number_one, number_two + 1, 1):
+    print(counter)
+
+            
