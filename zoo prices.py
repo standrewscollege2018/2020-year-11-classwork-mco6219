013d82e49c7fa68b3d69da732b4be8e90ae638d5
mco6219 <mco6219@stacmail.net>
2020-02-13 11:17:07 +1300

    Create zoo prices.py

---
+++ zoo prices.py
@@ -0,0 +1,15 @@
+""" this program determines whether you pay the child price at the zoo or not """
+
+# setting child age limit
+CHILD_AGE = 13
+
+# asking for user input
+age = int(input("How old are you?\n"))
+
+# seeing if the user is younger than CHILD_AGE
+if age < CHILD_AGE:
+    print("You pay the child price!")
+ 
+# print welcome   
+print("Welcome to the zoo!")
+
