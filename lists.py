b0841e15a37d619fc78e2d2dae974a2af6e85984
mco6219 <mco6219@stacmail.net>
2020-02-25 10:08:46 +1300

    Update lists.py

--- lists.py
+++ lists.py
@@ -6,3 +6,17 @@ fruit = ["apples", "bananas", "pineapple", "oranges"]
 
 # to print an item from a list, we use the name of the list and its index (which starts at 0)
 print(fruit[3])
+
+# to change an item, just use its index and set it directly
+fruit[0] = "mango"
+print(fruit)
+
+# to insert an item at a specific locstion, use insert(index, item)
+fruit.insert(3, "apples")
+print(fruit)
+
+# often it's just easier to add a new item to the end of the list
+# to do this we use append
+fruit.append("guavas")
+print(fruit)
+
