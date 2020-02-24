041ad4c69c8b04c385ae5bb47819f24f5678ceea
mco6219 <mco6219@stacmail.net>
2020-02-25 09:52:05 +1300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+
+""" this program demonstrates how lists work, including how to print, change and append items """
+
+# lists always have square brackets. items are separated by commas
+fruit = ["apples", "bananas", "pineapple", "oranges"]
+
+# to print an item from a list, we use the name of the list and its index (which starts at 0)
+print(fruit[3])
