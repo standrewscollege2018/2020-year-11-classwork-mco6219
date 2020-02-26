85544f690e9438b53470709d4f587564ab7cf4dc
mco6219 <mco6219@stacmail.net>
2020-02-27 11:12:11 +1300

    Create raffle.py

---
+++ raffle.py
@@ -0,0 +1,5 @@
+""" this program will run a simple raffle draw and produce a winner """
+
+print("Welcome to the raffle")
+raffle_item = input("Enter the raffle item that's up for grabs")
+raffle_value = input("Enter the raffle prize value")
