aadb69a4fe6d92a9d33054000b146d1d6cdde05e
mco6219 <mco6219@stacmail.net>
2020-03-03 09:49:00 +1300

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -1,5 +1,29 @@
 """ this program will run a simple raffle draw and produce a winner """
 
+# set list to store movies
+raffle = []
+
+# print intro and questions
 print("Welcome to the raffle")
-raffle_item = input("Enter the raffle item that's up for grabs")
-raffle_value = input("Enter the raffle prize value")
+raffle_item = input("Enter the raffle item that's up for grabs:\n")
+raffle_value = int(input("Enter the raffle prize value:\n"))
+
+# set boolean
+keep_asking = True
+
+
+while keep_asking == True:
+    name = input("What is your name to enter for the raffle?\n")
+    
+    if name == "end" or "stop":
+        keep_asking = False
+        
+    else:
+        raffle.append(name)
+        
+
+        
+
+    
+    
+        
