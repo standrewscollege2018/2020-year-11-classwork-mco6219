9d769b11205a539010a2109d682bb8060ea25ccc
mco6219 <mco6219@stacmail.net>
2020-03-03 10:17:13 +1300

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -2,26 +2,32 @@
 
 # set list to store movies
 raffle = []
-
+import random
 # print intro and questions
 print("Welcome to the raffle")
 raffle_item = input("Enter the raffle item that's up for grabs:\n")
 raffle_value = int(input("Enter the raffle prize value:\n"))
 
+stop_options = ["end", "stop"]
+
 # set boolean
 keep_asking = True
 
-
+# asking for names of entrants
 while keep_asking == True:
     name = input("What is your name to enter for the raffle?\n")
     
-    if name == "end" or "stop":
+    # if the name = end or stop then stop asking for another name
+    if name.lower() in stop_options:
         keep_asking = False
         
+    # if name doesn't equal end or stop then add the name to the list
     else:
         raffle.append(name)
-        
-
+ 
+# len(raffle) is asking how many items are in the list, and the -1 afterwards is saying how many items are in the list minus 1 equals the numbers randomint has to choose from   
+winner = random.randint(0,len(raffle)-1)
+print(raffle[winner])
         
 
     
