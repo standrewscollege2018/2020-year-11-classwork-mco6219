51d5c0afabfdc1dfa23e2d140ef21b11fcd9ac42
mco6219 <mco6219@stacmail.net>
2020-03-04 12:04:12 +1300

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -1,3 +1,19 @@
 """ this program will run a tournament with the competitiors names and opponents names and will score their points and results """
 
-# set list to store movies and say greeting
+# set list to store names and say greeting
+print("Welcome to the tournament")
+team_name = input("Enter your team name:\n")
+team_list = []
+
+# set boolean
+keep_asking = True
+
+
+
+# asking for names of opponents and set list to store opponents names
+while keep_asking == True:
+    opponent_names = input("Enter your opponents team names:\n")
+    opponent_list = []
+    
+
+
